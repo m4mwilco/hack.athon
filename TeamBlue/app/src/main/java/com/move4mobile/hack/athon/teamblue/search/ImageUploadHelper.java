1adf8af10aa18db00676891077d7479fccc80244
Wilco Wolters <wwolters@move4mobile.com>
2016-09-10 13:48:06 +0200

    Change app icon, scale images before upload

--- TeamBlue/app/src/main/java/com/move4mobile/hack/athon/teamblue/search/ImageUploadHelper.java
+++ TeamBlue/app/src/main/java/com/move4mobile/hack/athon/teamblue/search/ImageUploadHelper.java
@@ -1,12 +1,17 @@
 package com.move4mobile.hack.athon.teamblue.search;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.util.Log;
 
 import com.move4mobile.hack.athon.teamblue.RetrofitManager;
 import com.move4mobile.hack.athon.teamblue.network.model.Product;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -31,15 +36,14 @@ public class ImageUploadHelper implements ImageHelper.Listener {
 
     @Override
     public void onImageResult(Uri imageUri) {
-        InputStream inputStream = null;
+        byte[] content = new byte[0];
         try {
-            inputStream = context.getContentResolver().openInputStream(imageUri);
+            content = scaleImage(imageUri);
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
-
-        RequestBody requestFile = RequestBodyUtil.create(MediaType.parse("multipart/form-data"), inputStream);
+        RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), content);
         MultipartBody.Part body = MultipartBody.Part.createFormData("image[file]", "image.jpg", requestFile);
         RetrofitManager.getApiService().getProducts(body).enqueue(new Callback<List<Product>>() {
             @Override
@@ -58,6 +62,38 @@ public class ImageUploadHelper implements ImageHelper.Listener {
                 listener.onError();
             }
         });
+    }
+
+    private byte[] scaleImage(Uri file) throws IOException {
+
+
+        int targetW = 1000;
+        int targetH = 1000;
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        try (InputStream inputStream = fromUri(file)) {
+            BitmapFactory.decodeStream(inputStream);
+        }
+        int photoW = options.outWidth;
+        int photoH = options.outHeight;
+
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        options.inJustDecodeBounds = false;
+        options.inSampleSize = scaleFactor;
+
+        Bitmap scaledBitmap = null;
+        try (InputStream inputStream = fromUri(file)) {
+            scaledBitmap = BitmapFactory.decodeStream(inputStream, new Rect(), options);
+        }
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 70, outputStream);
+        return outputStream.toByteArray();
+    }
+
+    private InputStream fromUri(Uri uri) throws FileNotFoundException {
+        InputStream inputStream = context.getContentResolver().openInputStream(uri);        return inputStream;
     }
 
     public interface Listener {
